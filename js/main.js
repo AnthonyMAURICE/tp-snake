a8271df5ca60b9692619daa1863e87915a82aab2
AnthonyMAURICE <116059905+AnthonyMAURICE@users.noreply.github.com>
2024-07-09 12:31:11 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ const app = {
     data() {
         return {
             game: new Game(25, 30, new SnakeHead(10, 10), new Food())
-            // snake: new SnakeHead(10, 10),
         }
     },
     mounted(){
@@ -14,11 +13,10 @@ const app = {
         this.game.setSnakePresence()
         const body = document.querySelector("body");
         setInterval(this.move, 500)
-        setTimeout(this.foodDisplay, 5000)
+        setInterval(this.foodDisplay, 5000)
         body.onkeydown = this.setDir
     },
     methods: {
-
         setDir(e){
             this.game.snake.direction = e.code
         },
@@ -41,42 +39,11 @@ const app = {
             for(let elem of this.game.foodArray){
                 if(this.game.snake.getPosX() == elem.getPosX() && this.game.snake.getPosY() == elem.getPosY()){
                     elem.isEaten = true
+                    this.game.food.isOnGrid--
                     this.game.snake.grow()
                 }
             }
         }
-
-        // setSnakePresence(){
-        //     const gridelems = document.querySelectorAll('td')
-        //     for(let elem of gridelems){
-        //         elem.style.backgroundColor = 'white'
-        //         if(this.checkIfSnakeHead(elem)){
-        //             elem.style.backgroundColor = 'black'
-        //         }
-        //         for(let item of this.snake.bodyElem){
-        //             if(this.checkIfSnakeBody(item, elem)){
-        //                 elem.style.backgroundColor = 'black'
-        //             }
-        //         }
-        //     }
-        // },
-        // checkIfSnakeHead(_elem){
-        //     if(this.snake.getPosX() == _elem.dataset.posx && this.snake.getPosY() == _elem.dataset.posy){
-        //         return true
-        //     }else{
-        //         return false
-        //     }
-        // },
-        // checkIfSnakeBody(item, _elem){
-        //     if(item.getPosX() == _elem.dataset.posx && item.getPosY() == _elem.dataset.posy){
-        //         return true
-        //     }else{
-        //         return false
-        //     }
-        // },
-        // setDir(e){
-        //     this.snake.direction = e.code
-        // },
     }
 }
 
